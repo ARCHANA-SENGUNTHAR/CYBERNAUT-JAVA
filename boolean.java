74e1a2aa9c29ad23bfa603a76e8a1c41b23f6f8c
ARCHANA GURUSAMY <145648046+ARCHANA-SENGUNTHAR@users.noreply.github.com>
2024-12-27 23:25:40 +0530

    Add files via upload
    THIS IS ASG-2

---
+++ boolean.java
@@ -0,0 +1,14 @@
+//ASG-2
+/*
+**Question 2:**
+Create a Java program that declares a boolean variable called `isJavaFun`, 
+sets it to `true`, and prints its value with a comment indicating whether Java is fun or not. Not a conditional question.*/
+public class Main
+{
+	public static void main(String[] args) {
+	  //declaring and initializing boolean variable
+	   boolean isJavaFun=true;
+	    System.out.print("Java is fun to learn, do you feel it??");
+		System.out.print(isJavaFun);
+	}
+}
---
+++ double.java
@@ -0,0 +1,17 @@
+//ASG-2
+/*
+**Question 1:**
+Write a Java program that declares a double variable for 
+storing the price of an item, assigns it a value of 19.99, and then prints it with an appropriate comment.**/
+public class Main
+{
+	public static void main(String[] args) {
+	   //declaring a variable to store cost of an item
+	   double price;
+	   //to store cost of a cookies packet
+	   price=19.99;
+	    //displaying the cost
+		System.out.print("Cost of cookies pack purchased is:");
+		System.out.print(price);
+	}
+}
